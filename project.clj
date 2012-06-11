5be14eb68f9942b9a43f50aa5e130bbd35d5bdd2
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-06-11 17:19:28 +0200

    Switching to quartz-mongodb 1.0.0-beta2 and quartzite-rc6

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure             "1.3.0"]
                  [noir                            "1.3.0-beta7"]
-                 [com.novemberain/quartz-mongodb  "1.1.0-SNAPSHOT"]
-                 [clojurewerkz/quartzite          "1.0.0-SNAPSHOT"]
+                 [com.novemberain/quartz-mongodb  "1.1.0-beta2"]
+                 [clojurewerkz/quartzite          "1.0.0-rc6"]
                  [stencil                         "0.2.0"]
                  [org.clojure/data.json           "0.1.2"]]
   :resource-paths   ["src/resources"]
